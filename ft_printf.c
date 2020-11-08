241839d811ffbbe48e26d57e6ef7768c31672547
Junichi Tanaka <junichi200123@gmail.com>
2020-11-09 01:09:03 +0900

    parse flag

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,45 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 22:03:47 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/11/08 23:14:49 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/11/09 01:08:13 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include "ft_printf.h"
 
 // *formatは%の次の位置にポインタがある状態で渡される
-void parse_percent(const char *format)
+void parse_percent(const char *format, va_list ap)
 {
-	// フラグの解析
-	// 最小フィールド幅
-	// 精度
-	// 長さ修飾子
-	// 変換指定子 ('%'もここで処理する)
+	t_fmt *fmt_struct;
+	/* フラグの解析 */
+	/* 最小フィールド幅 */
+	if (ft_ispositive((char)*format))
+		fmt_struct->width = ft_atoi(format);
+	else if (*format == '*')
+	{
+		fmt_struct->width = va_arg(ap, size_t);
+		(format)++;
+	}
+
+	/* 精度 */
+	if (*format == '.')
+	{
+		if (ft_ispositive(*format))
+			fmt_struct->precision = ft_atoi(format);
+		else if (*format == '*')
+			fmt_struct->precision = va_arg(ap, size_t);
+		else
+			fmt_struct->precision = 0;
+		(format)++;
+	}
+	
+	
+	/* 長さ修飾子 */
+	
+	/* 変換指定子 ('%'もここで処理する) */
+
+	/* 出力 */
 }
 
 int ft_printf(const char *format, ...)
@@ -31,11 +56,9 @@ int ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format == '%')
-			parse_percent(++format);
+			parse_percent(++format, ap);
 		else
-		{
-		write(1, *format, 1);
-		}
+		write(1, format, 1);
 		format++;
 	}
 	va_end(ap);
