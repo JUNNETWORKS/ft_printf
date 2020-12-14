e6ac8c66bd91e48e6df2e7f1e75e8cd14992af4b
Junichi Tanaka <junichi200123@gmail.com>
2020-12-14 09:25:28 +0900

    refactor: ft_printf.h
    Signed-off-by: Junichi Tanaka <junichi200123@gmail.com>

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 21:55:18 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/12/14 09:23:23 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/12/14 09:25:07 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,23 +51,25 @@ typedef struct	s_fmt
 	long long	digit;
 }				t_fmt;
 
-t_fmt	*new_t_fmt();
-bool	is_unsigned_type(enum e_type type);
-bool	is_integer_type(enum e_type type);
-void	parse_flag(const char **format, t_fmt *fmt_data);
-void	parse_width(const char **format, t_fmt *fmt_data, va_list ap);
-void	parse_precision(const char **format, t_fmt *fmt_data, va_list ap);
-void	parse_type(const char **format, t_fmt *fmt_data);
-int		put_c_n_times(char c, int n);
-int		ft_printf(const char *format, ...);
-int		output_fmt(t_fmt *fmt_struct, va_list ap);
-int		output_fmt_nbr(char *num, t_fmt *fmt_data, int is_minus);
-int		fmt_itoa(long long n, t_fmt *fmt_data, char **num, long long len);
-int		write_char(t_fmt *fmt_data, va_list ap);
-int		write_string(t_fmt *fmt_data, va_list ap);
-int		write_integer(t_fmt *fmt_data, long long n);
-int		write_percent(t_fmt *fmt_data);
-int		write_fmt_data(t_fmt *fmt_data, va_list ap);
-int		parse_and_write(va_list ap, const char **format);
+t_fmt			*new_t_fmt();
+bool			is_unsigned_type(enum e_type type);
+bool			is_integer_type(enum e_type type);
+void			parse_flag(const char **format, t_fmt *fmt_data);
+void			parse_width(const char **format, t_fmt *fmt_data, va_list ap);
+void			parse_precision(const char **format,
+					t_fmt *fmt_data, va_list ap);
+void			parse_type(const char **format, t_fmt *fmt_data);
+int				put_c_n_times(char c, int n);
+int				ft_printf(const char *format, ...);
+int				output_fmt(t_fmt *fmt_struct, va_list ap);
+int				output_fmt_nbr(char *num, t_fmt *fmt_data, int is_minus);
+int				fmt_itoa(long long n, t_fmt *fmt_data,
+					char **num, long long len);
+int				write_char(t_fmt *fmt_data, va_list ap);
+int				write_string(t_fmt *fmt_data, va_list ap);
+int				write_integer(t_fmt *fmt_data, long long n);
+int				write_percent(t_fmt *fmt_data);
+int				write_fmt_data(t_fmt *fmt_data, va_list ap);
+int				parse_and_write(va_list ap, const char **format);
 
 #endif
